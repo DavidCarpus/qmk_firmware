81007bbd62e0594ae7aff73b8df3aee22da134bb
David Carpus <David.Carpus@analog.com>
2022-06-25 13:47:58 -0400

    Debugging oled

--- keyboards/handwired/dactyl_trackbeast/keymaps/dvorak/rules.mk
+++ keyboards/handwired/dactyl_trackbeast/keymaps/dvorak/rules.mk
@@ -17,3 +17,6 @@ BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 SPLIT_KEYBOARD = yes
+
+OLED_DRIVER_ENABLE = yes
+OLED_DRIVER = SSD1306
