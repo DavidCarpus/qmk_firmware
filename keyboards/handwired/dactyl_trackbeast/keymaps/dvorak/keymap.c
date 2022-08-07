bcf2436021cd944f23241c5f8c5f00ce77d5857f
David Carpus <David.Carpus@analog.com>
2022-08-07 16:27:32 -0400

    Maybe V1

--- keyboards/handwired/dactyl_trackbeast/keymaps/dvorak/keymap.c
+++ keyboards/handwired/dactyl_trackbeast/keymaps/dvorak/keymap.c
@@ -1,12 +1,6 @@
 #include QMK_KEYBOARD_H
 #include "config.h"
 
-#define KC_ML KC_MS_LEFT
-#define KC_MR KC_MS_RIGHT
-
-#define KC_MB1 KC_MS_BTN1
-#define KC_MB2 KC_MS_BTN1
-
 #define _LAYER0 0
 #define _LAYER1 1
 #define _LAYER2 2
@@ -14,28 +8,28 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_LAYER0] = LAYOUT(
-    KC_ESC, KC_QUOT, KC_COMM, KC_DOT, KC_P, KC_Y,                       KC_F, KC_G, KC_C, KC_R, KC_L, KC_MINS,
-    KC_TAB, KC_A, KC_O, KC_E, KC_U, KC_I,                               KC_D, KC_H, KC_T, KC_N, KC_S, KC_SLSH,
-    TT(1), KC_SCLN, KC_Q, KC_J, KC_K, KC_X,                             KC_B, KC_M, KC_W, KC_V, KC_Z, KC_BSLS,
-            LCTL_T(KC_LCBR), LALT_T(KC_RCBR), KC_MS_BTN1,KC_MS_BTN3 ,                  RCTL_T(KC_LPRN), RALT_T(KC_RPRN),
-                    LCTL(KC_S),  KC_LGUI ,                                KC_SFTENT ,  KC_SPC,
-                    KC_BSPC  , KC_DEL  ,                                  TT(1), KC_BSPC
+    QK_GESC, KC_QUOT, KC_COMM, KC_DOT, KC_P, KC_Y,                           KC_F, KC_G, KC_C, KC_R, KC_L, KC_MINS,
+    KC_TAB, KC_A, KC_O, KC_E, KC_U, KC_I,                                   KC_D, KC_H, KC_T, KC_N, KC_S, KC_SLSH,
+    TT(1), KC_SCLN, KC_Q, KC_J, KC_K, KC_X,                                 KC_B, KC_M, KC_W, KC_V, KC_Z, KC_BSLS,
+            KC_BTN1, KC_BTN3 , KC_LCPO, LALT_T(KC_LCBR),                RCTL_T(KC_RPRN), RALT_T(KC_RCBR),
+                    LCTL(KC_S),  KC_LGUI ,                                   KC_SFTENT ,  KC_SPC,
+                    KC_BSPC  , KC_DEL  ,                                     TT(2), KC_BSPC
     ),
 [_LAYER1] = LAYOUT(
-    KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,                       KC_RBRC, KC_5, KC_8, KC_9,KC_RSFT , KC_PLUS,
+    KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,                       KC_RBRC, KC_7, KC_8, KC_9,KC_RSFT , KC_PLUS,
     KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,                    KC_MINS, KC_4, KC_5, KC_6, KC_MINS, KC_PIPE,
-    TT(2), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,KC_LSFT,               KC_EQL, KC_1, KC_2, KC_3, KC_EQL, KC_UNDS,
-            LCTL(KC_D), KC_PSCR, KC_MS_BTN1, KC_BTN3,                                KC_0, KC_TRNS,
+    TT(2), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,KC_BTN8,               KC_EQL, KC_1, KC_2, KC_3, KC_EQL, KC_UNDS,
+            KC_TRNS, KC_TRNS, LCTL(KC_D), KC_PSCR,                                KC_0, KC_TRNS,
                             KC_UP, KC_PGUP,                     LCTL(KC_X),LCTL(KC_C) ,
-                            KC_DOWN, KC_PGDN,          TT(2), LCTL(KC_V)
+                            KC_DOWN, KC_PGDN,          TT(0), LCTL(KC_V)
     ),
 [_LAYER2] = LAYOUT(
     KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,               KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
     KC_TRNS, KC_LEFT, KC_UP, KC_DOWN, KC_RGHT, KC_LPRN,     KC_RPRN, KC_MPRV, KC_MPLY, KC_MNXT, KC_TRNS, KC_VOLU,
     TO(0), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLD,
-           KC_TRNS, KC_TRNS, KC_TRNS, QK_BOOT, /*QK_BOOT*/                    KC_EQL, KC_MUTE,
-                            KC_TRNS, KC_TRNS,                     KC_TRNS, QK_BOOT, /*QK_BOOT*/
-                            KC_TRNS, KC_TRNS,           TO(0), KC_TRNS
+           KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,                   KC_EQL, KC_MUTE,
+                           KC_MS_WH_UP , KC_BTN6,                     KC_TRNS, KC_TRNS,
+                           KC_MS_WH_DOWN , KC_BTN7,           TO(0), KC_TRNS
     )
 };
 
