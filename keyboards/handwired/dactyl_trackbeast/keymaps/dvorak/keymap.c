aedeb14cbdc84523ef4828da8d8c061f4d5ce2a2
David Carpus <David.Carpus@analog.com>
2022-08-09 15:12:10 -0400

    Add backtick key

--- keyboards/handwired/dactyl_trackbeast/keymaps/dvorak/keymap.c
+++ keyboards/handwired/dactyl_trackbeast/keymaps/dvorak/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,                       KC_RBRC, KC_7, KC_8, KC_9,KC_RSFT , KC_PLUS,
     KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,                    KC_MINS, KC_4, KC_5, KC_6, KC_MINS, KC_PIPE,
     TT(2), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,KC_BTN8,               KC_EQL, KC_1, KC_2, KC_3, KC_EQL, KC_UNDS,
-            KC_TRNS, KC_TRNS, LCTL(KC_D), KC_PSCR,                                KC_0, KC_TRNS,
+            KC_TRNS, KC_TRNS, LCTL(KC_D), KC_PSCR,                                KC_0, KC_GRV,
                             KC_UP, KC_PGUP,                     LCTL(KC_X),LCTL(KC_C) ,
                             KC_DOWN, KC_PGDN,          TT(0), LCTL(KC_V)
     ),
